9fed8d1b1c90b67da5eec6871015f2cc3dced910
Soňa Kehmova <sona.kehmova@mendelu.cz>
2025-04-24 13:47:37 +0200

    Initial commit

---
+++ script.R
@@ -0,0 +1,10 @@
+## https://ondrejmottl.github.io/VersionControl_Mendel_Apr2025/#/states-of-a-file
+
+#For existing project
+usethis::use_git()
+#Create new project with git tracking (either via RStudio GUI or via {usethis} package)
+
+usethis::create_project("<DIRECTORY>")
+# switch to the new project
+usethis::use_git()
+
