bcadd15adda3ef6be0c0501aa9d8639d6ef6d39f
Soňa Kehmova <sona.kehmova@mendelu.cz>
2025-04-24 13:49:55 +0200

    nově otevřeno

--- script.R
+++ script.R
@@ -1,7 +1,7 @@
 ## https://ondrejmottl.github.io/VersionControl_Mendel_Apr2025/#/states-of-a-file
 
 #For existing project
-usethis::use_git()
+!usethis::use_git()
 #Create new project with git tracking (either via RStudio GUI or via {usethis} package)
 
 usethis::create_project("<DIRECTORY>")
